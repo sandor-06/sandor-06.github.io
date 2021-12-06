3fbece530b6f73f1e1a0ce752d82da562847205a
sandor-06 <79028551+sandor-06@users.noreply.github.com>
2021-12-06 14:20:49 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,1 @@
     // create random background color for profile div
-        function random_bg_color() {
-        var profile = document.getElementById("profile");
-        var x = Math.floor(Math.random() * 222);
-        var y = Math.floor(Math.random() * 222);
-        var z = Math.floor(Math.random() * 222);
-        profile.style.background = "rgb(" + x + "," + y + "," + z + ")";
-        console.log(bgColor);}                                                                                                                     
-        random_bg_color();  
